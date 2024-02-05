05f494072c6869666e1352782cecad416ca3e5e8
rahulaitrich <rahul.aitrich@gmail.com>
2024-02-05 22:11:14 +0530

    editing in expense

--- src/Components/Expense/Expense.jsx
+++ src/Components/Expense/Expense.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import './Expense.css'
+import { FaArrowDownLong } from "react-icons/fa6";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { FaDollarSign } from "react-icons/fa";
+import { SlCalender } from "react-icons/sl";
+import { BiSolidMessageRounded } from "react-icons/bi";
+
+
 
 const Expense = () => {
     return (
@@ -8,11 +15,11 @@ const Expense = () => {
             <div class="expense_total">
                 <h2>Total Expense: <span>$1000</span></h2>
             </div>
-                <div className='inputvalue'>
+            <div className='inputvalue'>
                 <form>
-                    <input type="text" id="expenseTitle" placeholder="Expense Title"/><br />
-                    <input type="text" id="expenseAmount" placeholder="Expense Amount"/><br />
-                    <input type="date" id="expenseDate" placeholder="Enter A Date"/><br />
+                    <input type="text" id="expenseTitle" placeholder="Expense Title" /><br />
+                    <input type="text" id="expenseAmount" placeholder="Expense Amount" /><br />
+                    <input type="date" id="expenseDate" placeholder="Enter A Date" /><br />
                     <select id="expenseCategory">
                         <option>Select Option</option>
                         <option>Salary</option>
@@ -26,21 +33,35 @@ const Expense = () => {
                     </select><br />
                     <textarea placeholder="Add A Reference" cols="30" rows="10"></textarea><br />
                     <button type="submit" className='btn btn-danger'>+ Add Expense</button>
-                    </form>
-                </div>
-      
-            {/* <div className='scrollable'>
-            <div className="valueincome">
-                        <div className="icon">
-                        </div>
-                        <h2>Rent</h2><br />
-                        <h2> 1000</h2>
-                        <h2> 23/01/2024</h2>
-                        <h2> Rent And Bills</h2>
-                        <div className="icondelete">
-                        </div>
+                </form>
+            </div>
+
+            <div className='scrollable'>
+                <div className="valueincome">
+                    <div className="icon">
+                        <FaArrowDownLong />
                     </div>
-            </div> */}
+                    <h2>Rent</h2><br />
+                    <h2><FaDollarSign/>1000</h2>
+                    <h2><SlCalender/> 23/01/2024</h2>
+                    <h2><BiSolidMessageRounded/> Rent And Bills</h2>
+                    <div className="icondelete">
+                        <RiDeleteBin6Line />
+                    </div>
+                </div>
+                <div className="valueincome">
+                    <div className="icon">
+                        <FaArrowDownLong />
+                    </div>
+                    <h2>Dentist Appointment</h2><br />
+                    <h2><FaDollarSign/>2000</h2>
+                    <h2><SlCalender/>24/01/2024</h2>
+                    <h2><BiSolidMessageRounded/>Tooth Removal</h2>
+                    <div className="icondelete">
+                        <RiDeleteBin6Line />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
