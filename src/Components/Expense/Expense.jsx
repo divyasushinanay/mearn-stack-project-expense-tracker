eac5a1263941c2aae715ded06164befdd84752e2
rahulaitrich <rahul.aitrich@gmail.com>
2024-02-05 17:35:09 +0530

    editing in expense

--- src/Components/Expense/Expense.jsx
+++ src/Components/Expense/Expense.jsx
@@ -2,11 +2,47 @@ import React from 'react'
 import './Expense.css'
 
 const Expense = () => {
-  return (
-    <div className='expense'>
-        Expense
-    </div>
-  )
+    return (
+        <div className='expense'>
+            <h1>Expenses</h1>
+            <div class="expense_total">
+                <h2>Total Expense: <span>$1000</span></h2>
+            </div>
+                <div className='inputvalue'>
+                <form>
+                    <input type="text" id="expenseTitle" placeholder="Expense Title"/><br />
+                    <input type="text" id="expenseAmount" placeholder="Expense Amount"/><br />
+                    <input type="date" id="expenseDate" placeholder="Enter A Date"/><br />
+                    <select id="expenseCategory">
+                        <option>Select Option</option>
+                        <option>Salary</option>
+                        <option>Freelancing</option>
+                        <option>Investments</option>
+                        <option>Stocks</option>
+                        <option>Bitcoin</option>
+                        <option>Bank Transfer</option>
+                        <option>Youtube</option>
+                        <option>Other</option>
+                    </select><br />
+                    <textarea placeholder="Add A Reference" cols="30" rows="10"></textarea><br />
+                    <button type="submit" className='btn btn-danger'>+ Add Expense</button>
+                    </form>
+                </div>
+      
+            {/* <div className='scrollable'>
+            <div className="valueincome">
+                        <div className="icon">
+                        </div>
+                        <h2>Rent</h2><br />
+                        <h2> 1000</h2>
+                        <h2> 23/01/2024</h2>
+                        <h2> Rent And Bills</h2>
+                        <div className="icondelete">
+                        </div>
+                    </div>
+            </div> */}
+        </div>
+    )
 }
 
 export default Expense
