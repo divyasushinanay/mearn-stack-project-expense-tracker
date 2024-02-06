d04a599ee003a722f24a39f4926800d7e3a74b1d
rahulaitrich <rahul.aitrich@gmail.com>
2024-02-06 17:05:28 +0530

    editing in dashboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Dashboard.css';
-import img1 from '../../Imgs/images.png';
+import { AgChartsReact } from 'ag-charts-react';
 
 const Dashboard = ({ totalExpense, totalincome }) => {
+  // State to hold chart options
+  const [chartOptions, setChartOptions] = useState({
+    data: [
+      { category: 'Income', value: totalincome },
+      { category: 'Expense', value: totalExpense },
+    ],
+    series: [
+      {
+        type: 'bar',
+        xKey: 'category',
+        yKeys: ['value'],
+        fills: ['#3366cc', '#dc3912'],
+        strokes: ['#3366cc', '#dc3912'],
+      },
+    ],
+    // xAxis: {
+    //   type: 'category',
+    //   title: 'Category',
+    // },
+    // yAxis: {
+    //   min: 0,
+    //   title: 'Amount',
+    // },
+  });
+
+  // Update chartOptions when totalIncome or totalExpense changes
+  // useEffect(() => {
+  //   setChartOptions((prevOptions) => ({
+  //     ...prevOptions,
+  //     data: [
+  //       { category: 'Income', value: totalincome },
+  //       { category: 'Expense', value: totalExpense },
+  //     ],
+  //   }));
+  // }, [totalincome, totalExpense]);
+
   // Calculate total balance
   const totalBalance = totalincome - totalExpense;
 
@@ -11,23 +47,23 @@ const Dashboard = ({ totalExpense, totalincome }) => {
       <h1>All Transactions</h1>
       <div className='content'>
         <div className='graph'>
-          <img src={img1} alt="Graph"/>
+          <AgChartsReact options={chartOptions} className='graphs' />
         </div>
-        <div className="totalbalance">
+        <div className='totalbalance'>
           <h2>Total Balance</h2>
           <h2>${totalBalance}</h2>
         </div>
-        <div className="totalincome">
+        <div className='totalincome'>
           <h2>Total Income</h2>
           <h2>${totalincome}</h2>
         </div>
-        <div className="totalexpense">
+        <div className='totalexpense'>
           <h2>Total Expense</h2>
-          <h2>${totalExpense}</h2> 
+          <h2>${totalExpense}</h2>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
